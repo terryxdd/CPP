1c3ed1069a779a43ca4b8a4974bcb908fc2a804a
Di Xu <di.xu@horizon.ai>
2020-06-02 13:00:54 +0800

    6.1leetcode

--- longestPal.cpp
+++ longestPal.cpp
@@ -14,10 +14,18 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
+using namespace std; 
+
+int expand_center(string s, int left, int right) {
+  int L = left, R = right;
+  while (L >= 0 && R < s.length() && s[L] == s[R]) {
+    L--;
+    R++;
+  }
+  return R - L - 1;
+}
 
 string longestPalindrome(string s) {
-  
   if (s.length() < 1) {
     return "";
   }
@@ -34,16 +42,8 @@ string longestPalindrome(string s) {
   return s.substr(start, end - start + 1);
 }
 
-int expand_center(string s, int left, int right) {
-  int L = left, R = right;
-  while (L >= 0 && R < s.length() && s[L] == s[R]) {
-    L--;
-    R++;
-  }
-  return R - L - 1;
-}
-
 int main() {
   string pal_str = "abba";
-  std::cout << longestPalindrome(pal_str) << std::endl;
+  std::string res = longestPalindrome(pal_str);
+  std::cout << res << std::endl;
 }
